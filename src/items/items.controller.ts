f5c9f671909f7faf5d8dc76d21cbe8418eac571a
Kazunori Soetani <apple.email.address@gmail.com>
2026-01-10 23:54:39 +0900

    feat: implement database CRUD operations with Prisma

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,33 +1,33 @@
 import { Controller, Get, Post, Put, Delete, Body, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item.dto';
-import type { Item } from './items.model';
+import type { Item } from '@prisma/client';
 
 @Controller('items')
 export class ItemsController {
     constructor(private readonly itemsService: ItemsService) {}
     @Get()
-    findAll(): Item[] {
-        return this.itemsService.findAll();
+    async findAll(): Promise<Item[]> {
+        return await this.itemsService.findAll();
     }
 
     @Get(':id')
-    findById(@Param('id', ParseUUIDPipe) id: string): Item {
-        return this.itemsService.findById(id);
+    async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+        return await this.itemsService.findById(id);
     }
 
     @Post()
-    create(@Body() createItemDto: CreateItemDto): Item {
-        return this.itemsService.create(createItemDto);
+    async create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+        return await this.itemsService.create(createItemDto);
     }
 
     @Put(':id')
-    updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
-        return this.itemsService.updateStatus(id);
+    async updateStatus(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+        return await this.itemsService.updateStatus(id);
     }
 
     @Delete(':id')
-    delete(@Param('id', ParseUUIDPipe) id: string): void {
-        return this.itemsService.delete(id);
+    async delete(@Param('id', ParseUUIDPipe) id: string): Promise<Item> {
+        return await this.itemsService.delete(id);
     }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,42 +1,57 @@
 import { Injectable } from '@nestjs/common';
-import { Item } from './items.model';
+import { Item, ItemStatus } from '@prisma/client';
 import { CreateItemDto } from './dto/create-item.dto';
-import { v4 as uuid } from 'uuid';
 import { NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ItemsService {
-    private items: Item[] = [];
+    constructor(private readonly prismaService: PrismaService) {}
 
-    findAll(): Item[] {
-        return this.items;
+    async findAll(): Promise<Item[]> {
+        return await this.prismaService.item.findMany();
     }
 
-    findById(id: string): Item {
-        const item = this.items.find((item) => item.id === id);
+    async findById(id: string): Promise<Item> {
+        const item = await this.prismaService.item.findUnique({
+            where: { id },
+        });
         if (!item) {
             throw new NotFoundException(`Item with ID ${id} not found`);
         }
         return item;
     }
 
-    create(createItemDto: CreateItemDto): Item {
-        const item: Item = {
-            id: uuid(),
-            ...createItemDto,
-            status: 'ON_SALE',
-        };
-        this.items.push(item);
-        return item;
+    async create(createItemDto: CreateItemDto): Promise<Item> {
+        const { name, price, description } = createItemDto;
+        return await this.prismaService.item.create({
+            data: {
+                name,
+                price,
+                description,
+                status: ItemStatus.ON_SALE,
+            },
+        });
     }
 
-    updateStatus(id: string): Item {
-        const item = this.findById(id);
-        item.status = 'SOLD_OUT';
-        return item;
+    async updateStatus(id: string): Promise<Item> {
+        // 存在チェック（findById内で例外が投げられる）
+        await this.findById(id);
+        
+        // DBに更新を保存
+        return await this.prismaService.item.update({
+            data: { status: ItemStatus.SOLD_OUT },
+            where: { id },
+        });
     }
 
-    delete(id: string): void {
-        this.items = this.items.filter((item) => item.id !== id);
+    async delete(id: string): Promise<Item> {
+        // 存在チェック（findById内で例外が投げられる）
+        await this.findById(id);
+        
+        // DBから削除
+        return await this.prismaService.item.delete({
+            where: { id },
+        });
     }
 }
